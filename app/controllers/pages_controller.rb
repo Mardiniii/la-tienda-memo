3bd053ed6ec9d595cb442e6cdf048d4d67f1586a
mardiniii <sezama03@hotmail.com>
2016-11-18 19:47:43 -0500

    Fix category filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,11 @@ class PagesController < ApplicationController
 
   def home
     @categories = Category.all
+
     if params[:category_id].present?
       @products = Category.find(params[:category_id]).products
-      # @products = Product.where(category_id: params[:category_id])
     else
-      @products = Product.all.shuffle[0..5]
+      @products = @q.result(distinct: true)
     end
   end
 
